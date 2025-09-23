a10c58a54384d893671e372964442544e8655a29
SandeepReddyChalla <141617714+SandeepReddyChalla@users.noreply.github.com>
2025-09-23 23:49:47 +0530

    Update SqlQueries.sql

--- scripts/Silver/SqlQueries.sql
+++ scripts/Silver/SqlQueries.sql
@@ -1,4 +1,68 @@
+--creating empty silver tables
 
+CREATE TABLE silver.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+
+CREATE TABLE silver.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+
+--erp tables 
+
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
+GO
+
+CREATE TABLE silver.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
+GO
+
+CREATE TABLE silver.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
 
 -- Correction in Crm_cst_info 
 
@@ -38,34 +102,25 @@ INSERT INTO silver.crm_cust_info (
 		WHERE flag_last = 1;
 
 
-
-
-
-
 -- Correction on crm_prd_info table 
+--inserting the values into the silver.crm_prd_info after cleaning
+--while creating the data type for datetime for last 2 columns but we changed to Date.
 
-
-
-SELECT
-    prd_id,
-    REPLACE(SUBSTRING(prd_key, 1, 5), '.', '_') AS cat_id,
-    SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
-    prd_nm,
-    ISNULL(prd_cost, 0) AS prd_cost,
-    CASE UPPER(TRIM(prd_line))
-        WHEN 'M' THEN 'Mountain'
-        WHEN 'R' THEN 'Road'
-        WHEN 'S' THEN 'Other Sales'
-        WHEN 'T' THEN 'Touring'
-        ELSE 'n/a'
-    END AS prd_line,
-    CAST(prd_start_dt AS DATE) AS prd_start_dt,
-    CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
-FROM bronze.crm_prd_info
-
-
-
-
+IF OBJECT_ID('silver.crm_prd_info','U') is not null
+drop table silver.crm_prd_info;
+go
+CREATE TABLE silver.crm_prd_info (
+    prd_id       INT,
+	cat_id       NVARCHAR(50),
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME,
+	dwh_create_date DATETIME2 default GETDATE()
+);
+--inserting values into silver.crm_prd_info
 INSERT INTO silver.crm_prd_info (
 			prd_id,
 			cat_id,
